1a007ffccec7744ef97032b5b2bad707532407a1
Wences Kipsang <cheruiyotwences@gmail.com>
2024-09-11 17:11:23 +0300

    second commt

--- Django_CICD/wsgi.py
+++ Django_CICD/wsgi.py
@@ -1,11 +1,3 @@
-"""
-WSGI config for Django_CICD project.
-
-It exposes the WSGI callable as a module-level variable named ``application``.
-
-For more information on this file, see
-https://docs.djangoproject.com/en/5.1/howto/deployment/wsgi/
-"""
 
 import os
 
