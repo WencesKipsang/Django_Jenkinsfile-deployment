bca626891792c18d27faff4a7723045f737bf94c
Wences Kipsang <cheruiyotwences@gmail.com>
2024-09-13 16:35:10 +0300

    my fine

--- Django_CICD/urls.py
+++ Django_CICD/urls.py
@@ -4,8 +4,8 @@ from django.urls import path, include
 from cicd import urls as you
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('cicd/', include(you)),
+    path('frs-jenkins/admin/', admin.site.urls),
+    path('frs-jenkins/cicd/', include(you)),
 ]
 
 
